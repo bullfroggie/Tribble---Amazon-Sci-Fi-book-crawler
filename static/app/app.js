519bc9d38842fc00f9b16a5eb5edda411a06f3ad
Nikola Lausev <lausevnikola@protonmail.com>
2020-02-06 22:36:03 +0100

    Add state for results

--- static/app/app.js
+++ static/app/app.js
@@ -5,12 +5,19 @@
         "$stateProvider",
         "$urlRouterProvider",
         function($stateProvider, $urlRouterProvider) {
-            $stateProvider.state("search", {
-                url: "/",
-                templateUrl: "app/components/search/search.tpl.html",
-                controller: "SearchCtrl",
-                controllerAs: "sctrl"
-            });
+            $stateProvider
+                .state("search", {
+                    url: "/",
+                    templateUrl: "app/components/search/search.tpl.html",
+                    controller: "SearchCtrl",
+                    controllerAs: "sctrl"
+                })
+                .state("search.result", {
+                    url: "deals",
+                    templateUrl: "app/components/result/result.tpl.html",
+                    controller: "SearchCtrl",
+                    controllerAs: "sctrl"
+                });
 
             $urlRouterProvider.otherwise("/");
         }
