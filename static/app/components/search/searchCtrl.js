acf38931229a9b1d91742b8a30563c0fa5bd4e99
Nikola Lausev <lausevnikola@protonmail.com>
2020-02-06 22:36:34 +0100

    Add search template and controller

--- static/app/components/search/searchCtrl.js
+++ static/app/components/search/searchCtrl.js
@@ -3,11 +3,28 @@
 
     app.controller("SearchCtrl", [
         "$http",
-        function($http) {
+        "$state",
+        function($http, $state) {
             let that = this;
 
-            this.search = null;
+            this.searched = null;
+            this.deals = [];
+
+            this.search = function() {
+                if (that.searched == null || that.searched == "") {
+                    return;
+                } else {
+                    $http.get("api/search/" + that.searched).then(
+                        function(response) {
+                            console.log(response);
+                            $state.go("search.result");
+                        },
+                        function(reason) {
+                            console.log(reason);
+                        }
+                    );
+                }
+            };
         }
     ]);
-    
 })(angular);
